4a72178e6cfc89eaa96537f8d98f378b86d3e006
Jess Erickson <jess@spirographics.com>
2019-02-27 17:03:44 -0600

    Resigned to hellium

--- wp-content/plugins/musical-taxo/wilson_musicals.php
+++ wp-content/plugins/musical-taxo/wilson_musicals.php
@@ -64,6 +64,9 @@ License: GPL2
  		'exclude_from_search'   => false,
  		'publicly_queryable'    => true,
  		'capability_type'       => 'page',
+    'show_in_rest'          => true
+
+
  	);
  	register_post_type( 'mw_musical', $args );
 
